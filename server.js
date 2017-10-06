d63d3184af1945f7576e67e1097316515b3453c8
Andrew Bales <agbales@gmail.com>
2017-10-06 15:20:45 -0700

    bootstrap working

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.listen(3000, function() { console.log('listening')});
