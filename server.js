eb8339757c076b25f26c9730945a6fdcf3736eb4
Andrew Bales <agbales@gmail.com>
2017-10-09 23:01:40 -0700

    env port added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 8080;
 
 app.use('/', express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.listen(3000, function() { console.log('listening')});
+app.listen(port, function() { console.log('listening')});
